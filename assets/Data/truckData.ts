6cb69e81143886dc9ceb8f517f215ad37c658900
kauamana <53412681+kauamana@users.noreply.github.com>
2019-08-16 12:53:05 -0400

    Add files via upload

---
+++ assets/Data/truckData.ts
@@ -0,0 +1,8 @@
+import {truck} from '../../app/truck'
+
+		export const truckinfo:truck[]=[
+			{make:'Vitra ',style:'abc',engine:'mh6'},
+			{make:'Gold Plus',style:'def',engine:'dfgh6'},
+			{make:'Paper',style:'ghi',engine:'ghj5'},
+			{make:'Ply Centre',style:'jkl',engine:'fgh6'}
+		];
